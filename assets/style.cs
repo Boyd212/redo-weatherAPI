3343c4905795cffbc831fb200fbe111dffeb458b
unknown <matthewfive.30@gmail.com>
2023-04-17 18:26:13 -0500

    Undefined error

---
+++ assets/style.cs
@@ -0,0 +1,48 @@
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;600&display=swap");
+
+*{
+    box-size: border-box;
+}
+
+h1{
+    padding: 1rem
+    size: 2rem
+    font-family: sans-serif;
+    color: navy;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+body {
+    margin: 0
+    font-family: sans-serif;
+    background-color: navy;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: 100px;
+    flex-direction: column;
+}
+
+input {
+    padding: 1rem;
+    border-radius: 25px;
+    border: none;
+    background-color: #fff;
+    font-family: sans-serif;
+    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.5);
+    min-width: 200px;
+    font-size: 1.2rem;
+}
+
+.result {
+    text-align: center;
+    font-size: 1.5rem;
+}
+
+.result h2 {
+    margin-bottom: 0.5;
+    display: flex;
+    align-items: center;
+}
--- script.js
+++ script.js
@@ -1,3 +1,35 @@
-const url = "https://api.openweathermap.org/data/2.5/weather?q='+inputval.value+'&appid='+apik";
-const apiKey = "e2ebf5f91be9ed84ff7b3315f93cff5d";
+var url = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=" + apiKey;
+var apiKey = "e2ebf5f91be9ed84ff7b3315f93cff5d";
+var main = document.getElementById('main');
+var input = document.getElementById('input');
+var city = document.getElementById('city');
+//console.log(city);
+
+var url = (city)=> `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+
+async function getWeather(city){
+    var resp = await fetch(url(city), {
+        origin: "cros" });
+    var respData = await resp.json();
+    addWeather(respData);
+}
+
+function addWeather(data){
+    var temp = data.result;
+    var result = document.createElement('div')
+    result.classList.add('weather');
+    result.innerHTML = `<h2>${temp}°F </h2>
+        <small>${data.result}</small>`;
+
+    main.innerHTML= "";
+    main.appendChild(result);
+};
+
+city.addEventListener('submit',(x) =>{
+    x.preventDefault();
+    var location = input.value;
+    if(location){
+        getWeather(location)
+    } else {"Please enter a real city, or check your spelling!"}
+});
 
